2d60fc90f35f27846f594463030c8e7558a1fe30
LFelipeAlvarez <59030727+LFelipeAlvarez@users.noreply.github.com>
2022-01-04 11:03:33 -0500

    Update getAllCountries.js

--- src/helpers/getAllCountries.js
+++ src/helpers/getAllCountries.js
@@ -1,6 +1,6 @@
 export const getAllCountries = async () => {
     try {
-        const response = await fetch('https://restcountries.eu/rest/v3.1/all');
+        const response = await fetch('https://restcountries.com/v3.1/all');
         const data = await response.json();
         return data;
     } catch (error) {
